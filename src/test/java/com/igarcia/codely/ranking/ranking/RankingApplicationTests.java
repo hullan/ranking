e48f5ab19243d53791af07a3a9ab381b517854d6
Ignacio Garcia <inazio@gmail.com>
2021-11-27 11:08:52 +0100

    commit inicial

---
+++ src/test/java/com/igarcia/codely/ranking/ranking/RankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igarcia.codely.ranking.ranking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
